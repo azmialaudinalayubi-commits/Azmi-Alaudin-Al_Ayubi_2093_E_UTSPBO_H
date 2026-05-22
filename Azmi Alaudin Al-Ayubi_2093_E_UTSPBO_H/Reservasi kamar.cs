0290bbc6bf8539bc76803c459e21f68896cc5aa5
Azmi Alaudin <azmialaudinalayubi@gmail.com>
2026-05-22 20:04:14 +0700

    Add project files.

---
+++ Azmi Alaudin Al-Ayubi_2093_E_UTSPBO_H/Reservasi kamar.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azmi_Alaudin_Al_Ayubi_2093_E_UTSPBO_H
+{
+    public class Reservasi_kamar
+    {
+        private string namatamu;
+        private string idBooking;
+        private string TipeKamar;
+        protected int tanggalChekin;
+
+        public string Tipekamar { get; private set; }    
+    )
+    public Reservasi_kamar(string namatamu, string idBooking, string tipeKamar,int tanggalChekin)
+    {
+            this.namatamu = namatamu;
+            this.idBooking = idBooking; 
+            this.TipeKamar = tipeKamar;
+            this.tanggalChekin = tanggalChekin;
+    }
+
+     protected Reservasi_kamar(string namatamu, int tanggalChekin)
+        {
+            this.namatamu =namatamu; 
+            this.tanggalChekin=tanggalChekin;
+        }
+    
+
+    public string getnamatamu()
+    {
+            return namatamu;
+    }
+
+    public override double setnamatamu(string namatamu)
+    {
+        this.namatamu =namatamu;
+    }
+
+    public string getidBooking (string idBooking)
+    {
+        return idBooking;
+    }
+
+    public override setidBooking (string idBooking)
+    {
+        this.idBooking= idBooking;
+    }
+
+    public string getTipekamar (string Tipekamar)
+    {
+            return Tipekamar;
+    }
+
+    public void settanggalChekin(int tanggalChekin)
+    {
+            this.tanggalChekin = tanggalChekin;
+    }
+
+
+    public void Tampilaninfo()
+    {
+            Console.WriteLine("Nama Tamu : namatamu");
+            Console.WriteLine("id Booking : idBooking");
+            Console.WriteLine("Tipe kamar : TipeKamar");
+            Console.WriteLine("Tanggal Chekin : TanggalChekin");
+    }
+    
+}
---
+++ Azmi Alaudin Al-Ayubi_2093_E_UTSPBO_H/kamar standart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azmi_Alaudin_Al_Ayubi_2093_E_UTSPBO_H
+{
+    public class kamarstandart : Reservasi_kamar
+    {
+      private  int hargaPermalam;
+    }
+
+    public kamarstandart(int hargaPermalam, string namatamu, string idBooking, string TipeKamar, int tanggalChekin) : base (namatamu, idBooking, TipeKamar,tanggalChekin)
+    {
+            this.hargaPermalam = hargaPermalam;
+    }
+
+    public string gethargaPermalam()
+    {
+         return.hargaPermalam;
+    } 
+
+
+    public void sethargaPermalam()
+    {
+            this.hargaPermalam = hargaPermalam;
+    }
+
+    public string gettanggalChekin()
+    {
+            return.tanggalChekin;
+    }
+
+       
+    
+
+
+
+
+    
+    
+}
